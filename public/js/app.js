cafc36eab4becbf90972b1424fc2a5f06c6352b8
Muneebahmed1997 <61536319+Muneebahmed1997@users.noreply.github.com>
2020-06-02 12:14:05 +0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
+
 window.addEventListener('load', () => {
   // Chat platform
   const chatTemplate = Handlebars.compile($('#chat-template').html());
